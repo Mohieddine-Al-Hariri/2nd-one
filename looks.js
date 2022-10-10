9beec35072b6ff65da7cc77ce7e1dab4d0ae772f
Mohieddine-Al-Hariri <114818981+Mohieddine-Al-Hariri@users.noreply.github.com>
2022-10-10 15:43:18 +0300

    .

--- looks.js
+++ looks.js
@@ -1,3 +1,15 @@
-const item = document.querySelectorAll('#item');
-const addTO = item.querySelectorAll('.add-to-bag');
 
+
+let count = 0;
+const counter = document.getElementById('counter');
+const addToBag = document.querySelectorAll('#add-to-bag');
+addToBag.forEach(addToBagy=>addToBagy.addEventListener('click', event =>{
+    const cl = counter.classList;
+    const c = 'animated-counter';
+    count++;
+
+    counter.innerText = count;
+    cl.remove(c, cl.contains(c));
+    setTimeout(() => counter.classList.add('animated-counter'),1);
+})
+);
